fd162a51288acc2a2b262239e2e8b193c026ee6e
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-09-22 21:08:39 -0700

    working on tests

--- challenges/linkedList/linked-list.test.js
+++ challenges/linkedList/linked-list.test.js
@@ -1,1 +1,28 @@
 const { LinkedList } = require('./linked-list');
+
+describe('Linked list', () => {
+  it('starts empty list', () => {
+    const newList = new LinkedList();
+    const checking = {
+      head: null,
+      size: 0
+    };
+    expect(newList).toEqual(checking);
+  });
+
+  it('inserts node', () => {
+    const val = 'testing1';
+    const testList = new LinkedList();
+    testList.insert(val);
+    
+    expect(testList.size).toBe(1);
+  });
+
+  it('will return true if the value exists', () => {
+    const test = 'test';
+    const testList = new LinkedList();
+    
+    testList.insert(test);
+    expect(testList.includes(test)).toBe(true);
+  });
+});
