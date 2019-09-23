8fa70da31128030cec6f864356fd978aa0b58c9b
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-09-22 20:42:09 -0700

    Still working

--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -5,4 +5,44 @@ class Node {
   }
 }
 
-module.exports = Node;
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insert(value) {
+    const node = new Node(value);
+    this.head = node;
+    this.size++;
+    return this.head;
+  }
+
+  includes(value) {
+    let currentNode = this.head;
+    while(currentNode) {
+      if(currentNode.value === value) {
+        return true;
+      }
+      else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
+  }
+
+  toString() {
+    let currentNode = this.head;
+    let res = '';
+
+    while(currentNode) {
+      res = res + currentNode.value;
+      currentNode = currentNode.next;
+    }
+    return res;
+  }
+}
+
+module.exports = {
+  LinkedList
+};
