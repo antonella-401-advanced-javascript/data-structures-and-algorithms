dcf5474d52d5bdfe4a91f2afb878ef409f51bba2
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-09-29 11:41:13 -0700

    working on challenge

---
+++ challenges/stacksAndQueue/stacks-and-queue.test.js
@@ -0,0 +1,85 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    const node = new Node(value);
+
+    if(this.top === null) {
+      this.top = node;
+    }
+    else {
+      node.next = this.top;
+      this.top = node;
+    }
+  }
+
+  pop() {
+    if(this.top === null) {
+      console.log('no items to pop');
+    }
+
+    let element = this.top.value;
+    this.top = this.top.next;
+    return element;
+  }
+
+  peek() {
+    if(this.top === null) {
+      console.log('stack has no values to peek');
+    }
+
+    return this.top.value;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    if(this.front === null) {
+      this.front = node;
+    }
+    else {
+      let curr = this.front;
+      while(curr.next) {
+        curr = curr.next;
+      }
+      curr.next = node;
+    }
+  }
+
+  dequeue() {
+    let newFront;
+    if(this.front === null) {
+      console.log('queue is empty');
+    }
+    else {
+      newFront = this.front.value;
+      this.front = this.front.next;
+    }
+    return newFront;
+  }
+
+  peek() {
+    return this.front.value;
+  }
+}
+
+module.exports = {
+  Node,
+  Stack,
+  Queue
+};
