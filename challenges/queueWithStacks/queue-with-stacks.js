5641da69f1cc27da2dccb2cc514a68d8e88d0010
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-01 15:02:53 -0700

    done with class

---
+++ challenges/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,31 @@
+const { Stack } = require('../stacksAndQueue');
+
+class PseudoQueue {
+  constructor() {
+    this.stackIn = new Stack();
+    this.stackOut = new Stack();
+  }
+
+  enqueue(value) {
+    return this.stackIn.push(value);
+  }
+
+  dequeue() {
+    let element = this.inStack.top;
+    let poppedIn = this.inStack.pop();
+    let poppedOut = this.outStack.pop();
+
+    while(this.inStack.top) {
+      this.outStack.push(poppedIn);
+    }
+    this.inStack.pop();
+    while(this.outStack.peek()) {
+      this.inStack.push(poppedOut);
+    }
+    return element;
+  }
+}
+
+module.exports = {
+  PseudoQueue
+};
