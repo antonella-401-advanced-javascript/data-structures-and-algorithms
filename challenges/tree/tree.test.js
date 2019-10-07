461b8ae50f1e119185f0f06096d4ec2a39c137f3
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-07 10:01:05 -0700

    working on testing

--- challenges/tree/tree.test.js
+++ challenges/tree/tree.test.js
@@ -1,9 +1,30 @@
 const { Node, BinaryTree } = require('./tree');
 
 describe('Binary tree', () => {
+
   it('Can successfully instantiate an empty tree', () => {
     const emptyTree = new BinaryTree();
 
     expect(emptyTree.root).toBe(null);
+  });
+
+  it('Can successfully instantiate a tree with a single root node', () => {
+    const tree = new BinaryTree();
+
+    tree.add(1);
+
+    expect(tree.root.value).toBe(1);
+  });
+
+  it('Can successfully add a left child and right child to a single root node', () => {
+    const tree = new BinaryTree();
+
+    tree.add(5);
+    tree.add(3);
+    tree.add(7);
+
+    expect(tree.root.value).toBe(5);
+    expect(tree.root.left.value).toBe(3);
+    expect(tree.root.right.value).toBe(7);
   });
 });
