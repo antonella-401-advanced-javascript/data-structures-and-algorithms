6514d9b90326288d47a4bd1c11987a3f9e4ac958
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-21 21:55:23 -0700

    done with challenge

--- challenges/dateAdder/date-adder.test.js
+++ challenges/dateAdder/date-adder.test.js
@@ -1,8 +1,39 @@
 const add = require('./date-adder');
 
 describe('Date Addder', () => {
+
   it('adds seconds to date', () => {
     const testDate = Date.now();
     expect(add(testDate, '10s')).toBe(testDate + 10000);
+  });
+
+  it('adds minutes to date', () => {
+    const testDate = Date.now();
+    expect(add(testDate, '5m')).toBe(testDate + 300000);
+  });
+
+  it('adds hours to date', () => {
+    const testDate = Date.now();
+    expect(add(testDate, '1h')).toBe(testDate + 3600000);
+  });
+
+  it('adds days to date', () => {
+    const testDate = Date.now();
+    expect(add(testDate, '1d')).toBe(testDate + 86400000);
+  });
+
+  it('adds weeks to date', () => {
+    const testDate = Date.now();
+    expect(add(testDate, '1w')).toBe(testDate + 604800000);
+  });
+
+  it('adds months to date', () => {
+    const testDate = Date.now();
+    expect(add(testDate, '1M')).toBe(testDate + 18410112000);
+  });
+  
+  it('adds seconds to date', () => {
+    const testDate = Date.now();
+    expect(add(testDate, '1y')).toBe(testDate + 220921344000);
   });
 });
