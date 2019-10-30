0ef7a10549a58c8fcc8978d873a980ab5c4ec4f2
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-30 10:58:37 -0700

    think I am done with this challenge

--- challenges/oopsFiles/oops-files.js
+++ challenges/oopsFiles/oops-files.js
@@ -1,11 +1,68 @@
 const fs = require('fs').promises;
 
-const readDir = path => fs.readdir(path, 'utf8');
-const readFile = path => fs.readFile(path, 'utf8');
-const renameFile = (path, newPath) => fs.rename(path, newPath, 'utf8');
+const getNums = dir => {
+  return fs.readdir(dir)
+    .then(files => files.map(name => name.slice(0, name.length - 4)))
+    .catch(err => console.log(err));
+};
+
+const getPath = dir => {
+  return fs.readdir(dir)
+    .then(files => files.map(name => './folder/' + name))
+    .catch(err => console.log(err));
+};
+
+const getContent = arr => {
+  return Promise.all(arr.map(path => fs.readFile(path, { encoding: 'utf8' })))
+    .catch(err => console.log(err));
+};
+
+const getStamp = arr => {
+  return Promise.all(arr.map(path => {
+    return fs.stat(path)
+      .then(stat => stat.mtime)
+      .catch(err => console.log(err));
+  }))
+    .catch(err => console.log(err));
+};
+
+const rename = (paths, fileNumbers, contents, timestamps) => {
+  return Promise.all([paths.forEach((file, index) => {
+    fs.rename(file, `./folder/${contents[index]}-${fileNumbers[index]}-${timestamps[index]}`, err => {
+      if(err) {
+        throw err;
+      }
+    });
+  })])
+    .catch(err => console.log(err));
+};
+
+
+const fileRenamer = directory => {
+  let pathArr;
+  let numArr;
+  let contArr;
+  return getNums(directory)
+    .then(fileNumbers => (
+      numArr = fileNumbers,
+      getPath(directory)))
+    .then(filePaths => (
+      pathArr = filePaths,
+      getContent(filePaths)))
+    .then(fileContents => (
+      contArr = fileContents,
+      getStamp(pathArr)))
+    .then(timestamps => {
+      return rename(pathArr, numArr, contArr, timestamps);
+    })
+    .catch(err => console.log(err));
+};
+
+// fileRenamer('./challenges/oopsFiles/folder');
 
 module.exports = {
-  readDir,
-  readFile,
-  renameFile
+  getNums,
+  getPath,
+  getContent,
+  getStamp
 };
---
+++ challenges/oopsFiles/oops-files.test.js
@@ -0,0 +1,53 @@
+const { getNums, getPath, getContent, getStamp } = require('./oops-files');
+
+const folder = './folder';
+
+describe('File rename', () => {
+
+  it('gets an array of the file numbers', () => {
+    getNums(folder)
+      .then(fileNumbers => {
+        expect(fileNumbers).toEqual([
+          '0', '16', '22'
+        ]);
+      })
+      .catch(err => console.log(err));
+  });
+
+  it('gets an array of the file names', () => {
+    getPath(folder)
+      .then(files => {
+        expect(files).toEqual([
+          './folder/0.txt', './folder/16.txt',
+          './folder/22.txt'
+        ]);
+      })
+      .catch(err => err);
+  });
+
+  it('gets an array of the file contents', () => {
+    getPath(folder)
+      .then(files => {
+        return getContent(files);
+      })
+      .then(results => {
+        expect(results).toEqual([
+          'zero',
+          'sixteen',
+          'twenty-two',
+        ]);
+      })
+      .catch(err => err);
+  });
+
+  it('gets an array of the timestamps', () => {
+    getPath(folder)
+      .then(files => {
+        return getStamp(files)
+          .then(results => {
+            expect(results).toEqual(['2019-10-30T17:44:21.483Z', '2019-10-30T17:44:07.694Z', '2019-10-30T17:44:16.619Z']);
+          });
+      })
+      .catch(err => err);
+  });
+});
