d1983fe199287f7a09eaf3459a9e1900f6f6a48d
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-30 21:02:23 -0700

    test passes

--- challenges/insertionSort/insertion-sort.js
+++ challenges/insertionSort/insertion-sort.js
@@ -1,12 +1,13 @@
-const insertionSort = arr => {
+const insertionSort = (arr) => {
   for(let i = 1; i < arr.length; i++) {
     let j = i - 1;
     let temp = arr[i];
+    
     while(j >= 0 && temp < arr[j]) {
       arr[j + 1] = arr[j];
       j = j - 1;
     }
-    j = j - 1;
+    arr[j + 1] = temp;
   }
   return arr;
 };
