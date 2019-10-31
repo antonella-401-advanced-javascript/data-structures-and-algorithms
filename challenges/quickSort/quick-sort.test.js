63fbe9694a567a351037b70c2318efb85679357e
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-31 00:01:56 -0700

    passed missing params

--- challenges/quickSort/quick-sort.test.js
+++ challenges/quickSort/quick-sort.test.js
@@ -3,7 +3,9 @@ const quickSort = require('./quick-sort');
 describe('quick sort', () => {
   it('sorts array given', () => {
     const arr = [4, 3, 7, 1];
+    const right = (arr.length - 1);
+    const left = 0;
 
-    expect(quickSort(arr)).toEqual([1, 3, 4, 7]);
+    expect(quickSort(arr, left, right)).toEqual([1, 3, 4, 7]);
   });
 });
