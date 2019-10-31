8a7655d3bcf544f1220a7e156705140d4fbe9a18
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-30 21:22:41 -0700

    think I am done writing the code

--- challenges/mergeSort/merge-sort.js
+++ challenges/mergeSort/merge-sort.js
@@ -15,4 +15,32 @@ const mergeSort = (arr) => {
 const merge = (left, right, arr) => {
   let i = 0;
   let j = 0;
+  let k = 0;
+
+  while(1 < left.length && j < right.length) {
+    if(left[i] <= right[j]) {
+      arr[k] = left[i];
+      i = i + 1;
+    }
+    else {
+      arr[k] = right[j];
+    }
+    k = k + 1;
+  }
+
+  if(i === left.length) {
+    while(i < right.length) {
+      arr[k] = right[j];
+      k = k + 1;
+      i = i + 1;
+    }
+  }
+  else {
+    while(i < left.length) {
+      arr[k] = left[i];
+      k = k + 1;
+      i = i + 1;
+    }
+  }
+  return arr;
 };
