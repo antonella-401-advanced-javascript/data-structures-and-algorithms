57245ac1c04a0135ea8b14ea982604bd560a3590
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-30 20:51:14 -0700

    think I am done with code

--- challenges/insertionSort/insertion-sort.js
+++ challenges/insertionSort/insertion-sort.js
@@ -1,5 +1,14 @@
 const insertionSort = arr => {
   for(let i = 1; i < arr.length; i++) {
     let j = i - 1;
+    let temp = arr[i];
+    while(j >= 0 && temp < arr[j]) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    j = j - 1;
   }
+  return arr;
 };
+
+module.exports = insertionSort;
