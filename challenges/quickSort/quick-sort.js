ad535b45d7ab452f71428eb2213be305fbbf1c78
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-30 23:51:47 -0700

    working through pseudo code

--- challenges/quickSort/quick-sort.js
+++ challenges/quickSort/quick-sort.js
@@ -7,5 +7,15 @@ const quickSort = (arr, left, right) =>  {
 };
 
 const partition = (arr, left, right) => {
+  let pivot = arr[right];
+  let low = left - 1;
 
+  for(let i = left; i < right; i++) {
+    if(arr[i] <= pivot) {
+      low++;
+      swap(arr, i, low);
+    }
+    swap(arr, right, low + 1);
+    return low + 1;
+  }
 };
