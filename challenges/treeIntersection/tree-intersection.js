dcdf569022129dcc01915431c97ad6dddd6c7fca
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-06 20:07:00 -0800

    done with challenhe and README

--- challenges/treeIntersection/tree-intersection.js
+++ challenges/treeIntersection/tree-intersection.js
@@ -1,3 +1,16 @@
 const tree_intersection = (treeOne, treeTwo) => {
-  
+  const treeOneArr = treeOne.preOrder(treeOne.root);
+  const treeTwoArr = treeTwo.preOrder(treeTwo.root);
+  const result = [];
+
+  treeOneArr.forEach(i => {
+    treeTwoArr.forEach(j => {
+      if(i === j) {
+        result.push(i);
+      }
+    });
+  });
+  return result.sort();
 };
+
+module.exports = tree_intersection;
