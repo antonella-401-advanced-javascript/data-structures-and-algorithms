54279078269281917dc9a835b2053e550dc14832
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-10 18:54:22 -0800

    more testing

--- challenges/graph/graph.test.js
+++ challenges/graph/graph.test.js
@@ -4,6 +4,17 @@ describe('graph', () => {
   it('Adds a new node to the graph', () => {
     const test = new Graph();
     test.addNode('testing');
+
     expect(test.nodes.get('testing')).toEqual([]);
+  });
+
+  it('Adds a new edge between two nodes in the graph', () => {
+    const test = new Graph();
+    test.addNode('testing');
+    test.addNode('testing 2');
+    test.addEdge('testing', 'testing 2');
+
+    expect(test.nodes.get('testing')).toEqual(['testing 2']);
+    expect(test.nodes.get('testing 2')).toEqual(['testing']);
   });
 });
